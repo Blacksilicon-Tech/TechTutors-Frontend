74146e4882b371516d6e589ee8d7c6f5e2325285
Gozie55 <chigozieiwuchukwu@gmail.com>
2025-10-14 15:10:11 +0100

    Disabled webinar page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import FormPage from "./components/FormPage";
-import Webinar from "./components/Webinar";
+// import Webinar from "./components/Webinar";
 
 import "./App.css";
 import PaymentVerifyPage from "./components/PaymentVerifyPage";
@@ -14,7 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/form" element={<FormPage />} />
-        <Route path="/webinar" element={<Webinar />} />
+        
+        {/* <Route path="/webinar" element={<Webinar />} /> */}
 
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/payment/verify" element={<PaymentVerifyPage />} />
